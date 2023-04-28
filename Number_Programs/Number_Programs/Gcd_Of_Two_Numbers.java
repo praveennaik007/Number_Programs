4c67ddb17c6f80d9f88289b90f6098293a9ad151
praveennaik007 <126438461+praveennaik007@users.noreply.github.com>
2023-04-28 07:36:08 +0530

    Update Gcd_Of_Two_Numbers.java

--- Number_Programs/Number_Programs/Gcd_Of_Two_Numbers.java
+++ Number_Programs/Number_Programs/Gcd_Of_Two_Numbers.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	int num1=2;
 	int num2=4;
 	int gcd=0;
-	for (int i = 1; i <=num1%10&&i<=num2%10; i++) {
+	for (int i = 1; i <=num1&&i<=num2; i++) {
 		if(num1%i==0&&num2%i==0)
 		{
 			gcd=i;
